271e354b545fa5a20136a9559de252539f04c65e
EugeneBobin <umbobin@gmail.com>
2023-06-02 15:58:18 +0300

    HomeWork_8

---
+++ Roman to Integer.py
@@ -0,0 +1,16 @@
+def roman_to_integer(s):
+    roman_to_int = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    result = 0
+    prev_value = 0
+
+    for i in range(len(s) - 1, -1, -1):
+        current_value = roman_to_int[s[i]]
+        if current_value >= prev_value:
+            result += current_value
+        else:
+            result -= current_value
+        prev_value = current_value
+
+    return result
+
+print(roman_to_integer(input("Enter roman number :")))
